7e15c63eb58c681402e71b3ffdeb102b81aa044b
Galleazzo <paulogalleazzo1@gmail.com>
2025-10-10 22:57:30 -0300

    feat: criacao do microsserviço de criacao de pagamentos

---
+++ ms-save-payments/src/main/java/com/br/paymentsSave/MsSavePaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.br.paymentsSave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSavePaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSavePaymentsApplication.class, args);
+	}
+
+}
---
+++ ms-save-payments/src/test/java/com/br/paymentsSave/MsSavePaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.paymentsSave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSavePaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
