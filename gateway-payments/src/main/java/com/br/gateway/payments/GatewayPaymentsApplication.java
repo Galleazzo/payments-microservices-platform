85072a64635349d7866c5faaa8e0c0b714c19b44
Galleazzo <paulogalleazzo1@gmail.com>
2025-10-21 13:43:15 -0300

    feat: change route in gateway

--- gateway-payments/src/main/java/com/br/gateway/payments/GatewayPaymentsApplication.java
+++ gateway-payments/src/main/java/com/br/gateway/payments/GatewayPaymentsApplication.java
@@ -17,13 +17,8 @@ public class GatewayPaymentsApplication {
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(p -> p
-                        .path("/get")
-                        .filters(f -> f.addRequestHeader("Hello", "World"))
-                        .uri("http://httpbin.org:80"))
-                .route(p -> p
-                        .host("*.circuitbreaker.com")
-                        .filters(f -> f.circuitBreaker(config -> config.setName("mycmd").setFallbackUri("forward:/fallback")))
-                        .uri("http://httpbin.org:80")).
+                        .path("/api/payments")
+                        .uri("http://localhost:7074")).
                 build();
     }
 
