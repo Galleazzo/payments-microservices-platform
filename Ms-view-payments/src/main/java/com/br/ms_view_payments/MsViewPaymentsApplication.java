289520f0d8ffe4dd47bcafbf1222cb12dc4aa745
Galleazzo <paulogalleazzo1@gmail.com>
2026-02-21 15:14:25 -0300

    criacao de servico que ira salvar no mongo e retornar

---
+++ Ms-view-payments/src/main/java/com/br/ms_view_payments/MsViewPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.br.ms_view_payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsViewPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsViewPaymentsApplication.class, args);
+	}
+
+}
---
+++ Ms-view-payments/src/test/java/com/br/ms_view_payments/MsViewPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ms_view_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsViewPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
